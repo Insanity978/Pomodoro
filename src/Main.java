bb9fc3dad18784b05bbb80fe62e5ea8504c4f849
Insanity978 <52111627+Insanity978@users.noreply.github.com>
2025-02-10 12:22:50 -0500

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.print("How long do you want to study for? Type a number of minutes: ");
+        Scanner scanner = new Scanner(System.in);
+        int Study_minutes = scanner.nextInt();
+
+        System.out.println("How long should the break be? Type a number of minutes: ");
+        int Break_minutes = scanner.nextInt();
+
+        System.out.println("How many times do you want to repeat this? Enter a number: ");
+        int sessions = scanner.nextInt();
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
