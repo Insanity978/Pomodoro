2417b96879299470233a2a39b3e34e624813c8ef
Insanity978 <52111627+Insanity978@users.noreply.github.com>
2025-02-24 12:15:02 -0500

    Commit
    Stuff

--- src/TimerCore.java
+++ src/TimerCore.java
@@ -6,29 +6,46 @@ public class TimerCore {
     private int studyMinutes, breakMinutes, sessions;
     private boolean studyFinished = false;
     private long studyStartTime, studyEndTime;
+    private long breakStartTime, breakEndTime;
+    private boolean breakFinished;
 
     // Default constructor
     TimerCore() {
-        sessionPrompts();
+        studySessionPrompts();
         // need to add completed session counter
+        for(int i = 0; i < sessions; i++){
+            
+        }
         startStudy();
+        breakSessionPrompts();
+
     };
 
     /**
      * Prompts user for Pomodoro preferences
      */
-    public void sessionPrompts() {
+    public void studySessionPrompts() {
         userInput = new Scanner(System.in);
         System.out.print("How long do you want to study for? Type a number of minutes: ");
         studyMinutes = userInput.nextInt();
-
-        System.out.print("How long should the break be? Type a number of minutes: ");
-        breakMinutes = userInput.nextInt();
 
         System.out.print("How many times do you want to repeat this? Enter a number: ");
         sessions = userInput.nextInt();
 
         userInput.close();
+    }
+
+    public void breakSessionPrompts(){
+        userInput = new Scanner(System.in);
+        System.out.println("Would you like to start the break timer? y/n");
+        String x = userInput.nextLine().toLowerCase();
+        if(x.equals("y")){
+            System.out.println("How long do you want to take a break for?");
+            breakMinutes = userInput.nextInt();
+
+            userInput.close();
+
+        }
     }
 
     public void startStudy() {
@@ -47,7 +64,15 @@ public class TimerCore {
     }
 
     public void startBreak() {
-         
+        breakStartTime = System.currentTimeMillis(); //Starts when prompt ends @sessionPrompts
+        breakEndTime = breakStartTime + breakMinutes * 1000 * 60;
+
+        while(!breakFinished){
+            if(System.currentTimeMillis() >= breakEndTime) {
+                breakFinished = true;
+            }
+        }
+        System.out.println("finished study time!");
     }
     
     /**
